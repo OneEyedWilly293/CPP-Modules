96b7b005e58336a5320a488ea0374fa396a4fb2e
OneEyedWilly293clear <danielinkmou@gmail.com>
2026-01-21 17:27:42 +0200

    ex00 completed

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <cctype>
 
 int	main(int argc, char **argv)
 {
-	(void)argv;
+	int	i = 1;
+	int	j;
+
 	if (argc < 2)
+	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			std::cout << static_cast<char>(std::toupper(static_cast<unsigned char>(argv[i][j])));
+			j++;
+		}
+		i++;
+	}
+	std::cout << "\n";
 	return (0);
 }
