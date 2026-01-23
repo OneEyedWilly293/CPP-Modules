60a0823ea44b7b57193b9e90b9f30246c9b27095
OneEyedWilly293clear <danielinkmou@gmail.com>
2026-01-23 21:46:58 +0200

    Expansed on exercise 01

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -18,4 +18,51 @@ bool	Contact::setFromInput(void)
 		if (!std::getline(std::cin, _first_name))
 			return (false);
 	}
+
+	std::cout << "Last name: ";
+	if (!std::getline(std::cin,  _last_name))
+		return (false);
+	while (_last_name.empty())
+	{
+		std::cout << "Last name cannot be empty. Try again: ";
+		if (!std::getline(std::cin, _last_name))
+			return (false);
+	}
+
+	std::cout << "Nickname: ";
+	if (!std::getline(std::cin, _nickname))
+		return (false);
+	while (_nickname.empty())
+	{
+		std::cout << "Nickname cannot be empty. Try again: ";
+		if (!std::getline(std::cin, _nickname))
+			return (false);
+	}
+
+	std::cout << "Phone number: ";
+	if (!std::getline(std::cin, _phone_number))
+		return (false);
+	while (_phone_number.empty())
+	{
+		std::cout << "Phone number cannot be emty. Try again: ";
+		if (!std::getline(std::cin, _phone_number))
+			return (false);
+	}
+
+	std::cout << "Darkest secret: ";
+	if (!std::getline(std::cin, _darkest_secret))
+		return (false);
+	while (_darkest_secret.empty())
+	{
+		std::cout << "Darkest secret cannot be empty. Try again: ";
+		if (!std::getline(std::cin, _darkest_secret))
+			return (false);
+	}
+	return (true);
 }
+
+std::string	Contact::getFirstName(void) const { return (_first_name); }
+std::string	Contact::getLastName(void) const { return (_last_name); }
+std::string	Contact::getNickname(void) const { return (_nickname); }
+std::string	Contact::getPhoneNumber(void) const { return (_phone_number); }
+std::string	Contact::getDarkestSecret(void) const { return (_darkest_secret); }
