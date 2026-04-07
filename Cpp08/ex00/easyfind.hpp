53696d1bb0205f35b36d74812d92bc41b6ab7fc1
OneEyedWilly <danielinkmou@gmail.com>
2026-04-08 02:57:49 +0300

    Added notes and updated the test file format

--- Cpp08/ex00/easyfind.hpp
+++ Cpp08/ex00/easyfind.hpp
@@ -3,6 +3,17 @@
 #include <algorithm> // std::find
 #include <stdexcept> // std::runtime_error
 
+/*
+ * "typename T::iterator" is the return type.
+ *	- Every STL container has a nested type called "interator".
+ *	- e.g., std::vector<int>::iterator, std::list<int>::iterator
+ *	- We use "typename" here to tell the compiler: "this IS a type" (to prevent panic)
+ *	- By default(compiler) assumes it's a variable(which breaks everything).
+ *
+ * Parameters:
+ * 	T& container	-> a reference to any container of integers
+ *  int value		-> the number we're searching for
+ * */
 template <typename T>
 typename T::iterator easyfind(T& container, int value)
 {
@@ -14,3 +25,18 @@ typename T::iterator easyfind(T& container, int value)
 
 	return it;
 }
+
+
+/*
+ * std::find scans the range [container.begin(), container.end())
+ * and returns an iterator pointing to the FIRST match.
+ *
+ * If the value is NOT found, it returns container.end().
+ * container.end() is NOT a valid element - it's a sentinel; "past the end of the container."
+ *
+ * If value IS found
+ * We return the iterator pointing directly to that element.
+ * The caller can derefence it with *it to get the value.
+ *
+ * https://en.cppreference.com/w/cpp/algorithm/find.html
+ * */
--- Cpp08/ex00/main.cpp
+++ Cpp08/ex00/main.cpp
@@ -2,15 +2,35 @@
 #include <vector>
 #include <list>
 #include <deque>
+#include <string>
 #include "easyfind.hpp"
+
+std::ostream& bold_on(std::ostream& os)
+{
+	return os << "\e[1m";
+}
+
+std::ostream& bold_off(std::ostream& os)
+{
+	return os << "\e[0m";
+}
+
+void printHeader(const std::string& title)
+{
+	size_t width = title.size() + 8;
+
+	std::string border(width, '=');
+
+	std::cout << bold_on << border << "\n";
+	std::cout << bold_on << "=   " << title << "   =" << "\n";
+	std::cout << bold_on << border << "\n" << bold_off;
+}
 
 int main()
 {
 	// TEST 1: std::vector
 	std::vector<int> vec = {10, 20, 30, 40, 50};
-
-	std::cout << "=== Testing std::vector ===" << std::endl;
-
+	printHeader("TEST 1 std::vector");
 	try
 	{
 		auto it = easyfind(vec, 30);
@@ -22,6 +42,7 @@ int main()
 	}
 
 	// Test not found - should trigger the exception
+	std::cout << "\nSearching for value(exception): 99" << std::endl;
 	try
 	{
 		auto it = easyfind(vec, 99);
@@ -34,8 +55,8 @@ int main()
 
 	// TEST 2: std::list
 	std::list<int> lst = {100, 200, 300};
-
-	std::cout << "\n=== Testing std::list ===" << std::endl;
+	std::cout << "\n";
+	printHeader("TEST 2 std::list");
 
 	try
 	{
@@ -49,8 +70,8 @@ int main()
 
 	// TEST 3: std::deque
 	std::deque<int> dq = {7, 8, 9};
-
-	std::cout << "\n=== Testing std::deque ===" << std::endl;
+	std::cout << "\n";
+	printHeader("TEST 3 std::deque");
 
 	try
 	{
