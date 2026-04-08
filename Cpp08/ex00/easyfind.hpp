73d4fd922d4a7dded24b41f8d36fb1fc9e599fd0
OneEyedWilly <danielinkmou@gmail.com>
2026-04-08 03:39:52 +0300

    Added a overload for const iterator && added test for const_iterator

--- Cpp08/ex00/easyfind.hpp
+++ Cpp08/ex00/easyfind.hpp
@@ -15,6 +15,8 @@
  *  int value		-> the number we're searching for
  * */
 template <typename T>
+// OVERLOAD 1: Non-const container -> returns a regular iterator
+// Used when the container is NOT const, allowing you to modify elements.
 typename T::iterator easyfind(T& container, int value)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(),
@@ -26,6 +28,21 @@ typename T::iterator easyfind(T& container, int value)
 	return it;
 }
 
+// OVERLOAD 2: Const container -> returns a const_iterator
+// Used when the container IS const (read-only).
+// "const T&" means we accept the container as read-only.
+// "T::const_iterator" is the read-only iterator type every STL container has.
+template <typename T>
+typename T::const_iterator easyfind(const T& container, int value)
+{
+	typename T::const_iterator it = std::find(container.begin(), container.end(),
+			value);
+
+	if (it == container.end())
+		throw std::runtime_error("easyfind: value not found in container");
+
+	return it;
+}
 
 /*
  * std::find scans the range [container.begin(), container.end())
@@ -37,6 +54,9 @@ typename T::iterator easyfind(T& container, int value)
  * If value IS found
  * We return the iterator pointing directly to that element.
  * The caller can derefence it with *it to get the value.
+ *
+ * On a const container, .begin() and .end() automatically
+ * return const_iterators - you cannot modify elements through them.
  *
  * https://en.cppreference.com/w/cpp/algorithm/find.html
  * */
--- Cpp08/ex00/main.cpp
+++ Cpp08/ex00/main.cpp
@@ -83,5 +83,43 @@ int main()
 		std::cout << "Exception: " << e.what() << std::endl;
 	}
 
+	// TEST 4: const container (const_iterator)
+	// This test verifies that easyfind works on a READ-ONLY (const) container.
+	// A const container forces the compiler to use "const_iterator" instead of
+	// "iterator". If your easyfind only handles T&, this test won't even compile.
+	std::cout << "\n";
+	printHeader("Testing const std::vector");
+
+	// The container is const - its values can NEVER be modified after this line.
+	const std::vector<int> constVec = {3, 14, 15, 92, 65, 35};
+	std::vector<int> constTests = {14, 92, 0, 7, 65, 100};
+
+	/* ranged-based for loop(C++11)
+	 * val: a fresh variable created to hold each element, one at a time
+	 * ":" - the colon means "in"; read as "for each val IN constTests"
+	 * constTests: The container to loop through
+	 *
+	 * Old way: manual index, verbose
+	 * for (size_t i = 0; i < constTests.size(); i++)
+	 * {
+	 * 		int val = constTests[i];
+	 * 		// use val...
+	 * }
+	 */
+	for (int val : constTests)
+	{
+		try
+		{
+			// We can dereference it to READ the value, but we cannot write to it.
+			// e.g. *it = 99 would be a compile error(which is correct behaviour).
+			typename std::vector<int>::const_iterator it = easyfind(constVec, val);
+			std::cout << "Found: " << *it << "\n";
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << "Error: " << e.what() << '\n';
+		}
+	}
+
 	return 0;
 }
