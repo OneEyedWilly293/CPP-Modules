5fdbea0ff1188508ede3accf9ef6e9fb301b6303
OneEyedWilly <danielinkmou@gmail.com>
2026-06-03 23:31:30 +0300

    complete ex01

---
+++ Cpp09/ex01/RPN.cpp
@@ -0,0 +1,81 @@
+#include "RPN.hpp"
+#include <stdexcept>
+
+RPN::RPN()
+{
+}
+
+RPN::RPN(const RPN &other) : _stack(other._stack)
+{
+}
+
+RPN &RPN::operator=(const RPN &other)
+{
+	if (this != &other)
+		_stack = other._stack;
+	return *this;
+}
+
+RPN::~RPN()
+{
+}
+
+bool RPN::isOperator(char c) const
+{
+	return c == '+' || c == '-' || c == '*' || c == '/';
+}
+
+int RPN::applyOperator(char op, int a, int b) const
+{
+	if (op == '+')
+		return a + b;
+	if (op == '-')
+		return a - b;
+	if (op == '*')
+		return a * b;
+	if (op == '/')
+	{
+		if (b == 0)
+			throw std::runtime_error("Error: division by zero");
+		return a / b;
+	}
+	throw std::runtime_error("Error: unknown operator");
+}
+
+int RPN::evaluate(const std::string &expression)
+{
+	while (!_stack.empty())
+		_stack.pop();
+
+	for (auto token : expression)
+	{
+		if (token == ' ')
+			continue;
+
+		if (std::isdigit(token))
+		{
+			_stack.push(token - '0');
+		}
+		else if (isOperator(token))
+		{
+			if (_stack.size() < 2)
+				throw std::runtime_error("Error: invalid expression");
+
+			int b = _stack.top();
+			_stack.pop();
+
+			int a = _stack.top();
+			_stack.pop();
+
+			_stack.push(applyOperator(token, a, b));
+		}
+		else
+		{
+			throw std::runtime_error("Error: invalid token in expression");
+		}
+	}
+	if (_stack.size() != 1)
+		throw std::runtime_error("Error: invalid expression");
+
+	return _stack.top();
+}
--- Cpp09/ex01/RPN.hpp
+++ Cpp09/ex01/RPN.hpp
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <string>
+#include <stack>
+
+class RPN
+{
+	public:
+		RPN();
+		RPN(const RPN &other);
+		RPN &operator=(const RPN &other);
+		~RPN();
+
+		int evaluate(const std::string &expression);
+
+	private:
+		std::stack<int> _stack;
+		bool isOperator(char c) const;
+		int applyOperator(char op, int a, int b) const;
+};
